b0e1f49851acaf0fda8e00db8cc1f13a771f2f43
rxu <rxu@mail.ru>
2020-02-09 21:30:33 +0700

    Add simple test

---
+++ tests/functional/viewonline_test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*
+* Detailed Viewonline extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace rxu\DetailedViewonline\tests\functional;
+
+/**
+ * @group functional
+ */
+class viewonline_test extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('rxu/DetailedViewonline');
+	}
+
+	public function test_viewonline_page()
+	{
+		$this->login();
+
+		self::request('GET', "viewonline.php?sg=1&sk=b&sd=d&start=0");
+	}
+}
